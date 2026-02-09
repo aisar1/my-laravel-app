e3c228abd269c16e9b738205c791e013ed287658
Aisar <aisarmuhd3@gmail.com>
2026-02-09 18:43:13 +0800

    change to password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,7 +47,7 @@
                         type="password" 
                         name="password" 
                         id="password" 
-                        value="asyikin1password"
+                        value="password"
                         required 
                         class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition duration-200 pr-10"
                         placeholder="••••••••"
