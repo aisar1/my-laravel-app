618cd9d52f48e1e1ed1be515100be330f0d68c61
Aisar <aisarmuhd3@gmail.com>
2026-02-10 12:07:04 +0800

    change email field to aisar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,7 +29,7 @@
                     type="email" 
                     name="email" 
                     id="email" 
-                    value="asyikin@infinix.com" 
+                    value="aisar@infinix.com" 
                     required 
                     autofocus
                     class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition duration-200"
